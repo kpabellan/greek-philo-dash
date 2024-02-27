f964b2dd37b83e7966405e00b65d3e5c6eda3277
Kobey Pabellan <pabellank@gmail.com>
2024-02-27 01:57:49 -0800

    Added logo

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
     <>
       <header className="flex justify-between items-center">
         <div className="flex items-center">
-          <img src="/images/huntsman.jpg" alt="Sigma Chi Logo" className="h-20" />
+          <img src="/images/derbydays.svg" alt="Sigma Chi Logo" className="h-20" />
         </div>
         <p className="ml-auto">ΣX Derby Days '24</p>
         <hr className="text-white" />
