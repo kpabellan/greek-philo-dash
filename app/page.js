df49d94cebb4f30f8dad7c2fd9987b0819a8f62c
Kobey Pabellan <pabellank@gmail.com>
2024-03-07 02:17:17 -0800

    Page styling

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Activities from "../components/Activities";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-10">
+    <main className="flex min-h-screen flex-col items-center pt-10">
       <Leaderboard />
       <Activities />
     </main>
