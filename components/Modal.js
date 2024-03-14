03b95c063f29a6a02b13d998d61bf65fcac819bf
Kobey Pabellan <pabellank@gmail.com>
2024-03-14 01:36:58 -0700

    Modal content

--- components/Modal.js
+++ components/Modal.js
@@ -4,7 +4,9 @@ function Modal({ isOpen, onClose }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData();
-    const fileField = document.querySelector('input[type="file"]');
+    const fileField = document.querySelector('input[type="file"]')
+    const nameField = document.querySelector('input[name="name"]')
+    const organizationField = document.querySelector('select[name="orgs"]')
 
     formData.append('file', fileField.files[0]);
 
@@ -14,8 +16,8 @@ function Modal({ isOpen, onClose }) {
     })
       .then(response => response.json())
       .then(data => {
-        if (data.secure_url) {
-          console.log('Upload successful', data.secure_url);
+        if (data.url) {
+          console.log('Upload successful');
           onClose();
         }
       })
@@ -32,8 +34,38 @@ function Modal({ isOpen, onClose }) {
         <h2 className="text-xl mb-4">Submit a Photo</h2>
         <form onSubmit={handleSubmit}>
           <div>
-            <label>Image File</label>
-            <input type="file" name="file" required className="border rounded p-2 w-full bg-white text-black" />
+            <div>
+              <label>Image File</label>
+              <input type="file" name="file" required className="border rounded p-2 w-full h-12 bg-white text-black" />
+            </div>
+
+            <div>
+              <label>Name</label>
+              <input type="text" name="name" required className="border rounded p-2 w-full h-12 text-xl  bg-white text-black" />
+            </div>
+
+            <div>
+              <label>Organization</label>
+              <select id="orgs" name="orgs" className="border rounded p-2 w-full h-12 text-xl bg-white text-black">
+                <option value="Delta Delta Delta">ΔΔΔ</option>
+                <option value="Delta Gamma">ΔΓ</option>
+                <option value="Kappa Kappa Gamma">ΚΚΓ</option>
+                <option value="Phi Mu">ΦΜ</option>
+                <option value="Theta Tau">ΘΤ</option>
+                <option value="Delta Sigma Pi">ΔΣΠ</option>
+                <option value="Alpha Kappa Psi">ΑΚΨ</option>
+                <option value="Alpha Phi Omega">ΑΦΩ</option>
+                <option value="Delta Epsilon Mu">ΔΕΜ</option>
+                <option value="Phi Alpha Delta">ΦΑΔ</option>
+                <option value="Phi Delta Epsilon">ΦΔΕ</option>
+                <option value="Pi Kappa Phi">ΠΚΦ</option>
+                <option value="Kappa Sigma">ΚΣ</option>
+                <option value="Lambda Theta Nu">ΛΘΝ</option>
+                <option value="Sigma Theta Psi">ΣΘΨ</option>
+                <option value="Kappa Delta Chi">ΚΔΧ</option>
+              </select>
+            </div>
+
             <div className="mt-4 flex justify-end">
               <button type="button" onClick={onClose} className="mr-2">Cancel</button>
               <button type="submit">Submit</button>
