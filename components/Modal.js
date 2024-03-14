b3a8520de75f11015a91c81a08b38bda0559564c
Kobey Pabellan <pabellank@gmail.com>
2024-03-14 03:22:15 -0700

    Toasts

--- components/Modal.js
+++ components/Modal.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useToast } from '../context/Toast';
 
 function Modal({ isOpen, onClose }) {
+  const { showToast } = useToast();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData();
-    const fileField = document.querySelector('input[type="file"]')
-    const nameField = document.querySelector('input[name="name"]')
-    const organizationField = document.querySelector('select[name="orgs"]')
+    const fileField = document.querySelector('input[type="file"]');
+    const nameField = document.querySelector('input[name="name"]');
+    const organizationField = document.querySelector('select[name="orgs"]');
 
     formData.append('file', fileField.files[0]);
 
@@ -14,15 +17,24 @@ function Modal({ isOpen, onClose }) {
       method: 'POST',
       body: formData,
     })
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
       .then(data => {
         if (data.url) {
           console.log('Upload successful');
+          showToast('Photo submitted successfully', 'success');
           onClose();
+        } else {
+          throw new Error('The response does not contain a URL');
         }
       })
       .catch((error) => {
         console.error('Upload failed', error);
+        showToast('Error submitting photo', 'error');
       });
   };
 
@@ -46,7 +58,8 @@ function Modal({ isOpen, onClose }) {
 
             <div>
               <label>Organization</label>
-              <select id="orgs" name="orgs" className="border rounded p-2 w-full h-12 text-xl bg-white text-black">
+              <select id="orgs" name="orgs" className="border rounded p-2 w-full h-12 text-xl bg-white text-black" required>
+                <option value="" disabled selected>Select your organization</option>
                 <option value="Delta Delta Delta">ΔΔΔ</option>
                 <option value="Delta Gamma">ΔΓ</option>
                 <option value="Kappa Kappa Gamma">ΚΚΓ</option>
